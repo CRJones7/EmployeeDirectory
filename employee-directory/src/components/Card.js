856ba538a2ba493cae8602184c477a670baf9230
Courtney Jones <courtneyrjones95@gmail.com>
2020-08-14 16:50:25 -0500

    styled card

--- employee-directory/src/components/Card.js
+++ employee-directory/src/components/Card.js
@@ -1,5 +1,23 @@
 import React from "react";
 
+const styles = {
+    card: {
+        marginBottom: 30,
+        display: "flex",
+        justifyContent: "center",
+        maxHeight: "700px",
+        overflow: "scroll"
+
+    },
+    heading: {
+        background: "#9a74db",
+        minHeight: 50,
+        lineHeight: 3.5,
+        fontSize: 80,
+        color: "white",
+        padding: "0 20px"
+    }
+};
 
 function Card(props) {
     return (
