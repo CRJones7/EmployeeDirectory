a1d5ef15bf3e21de07da4f5671c665043d137bc5
Courtney Jones <courtneyrjones95@gmail.com>
2020-08-14 16:51:07 -0500

    styled header

--- employee-directory/src/components/Title.js
+++ employee-directory/src/components/Title.js
@@ -1,5 +1,24 @@
 import React from "react";
 
+const styles = {
+    Title: {
+        fontSize: "60px",
+        backgroundColor: "#0077b6",
+        marginTop: 0,
+        marginBottom: 30,
+        height: "100px",
+        display: "flex",
+        justifyContent: "center",
+
+    },
+    head: {
+        backgroundColor: "#023e8a",
+        margin: 0,
+
+
+    }
+
+}
 
 function Title() {
     return (
